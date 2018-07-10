3370039660023e00ed4c4e489db1b88172702c5b
max#237 <tmguil2015inde@gmail.com>
2018-07-10 22:20:43 +0530

    Class
    from function component to ES6

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function App(props) {
-  return (
-    <div className="App">
-      <h1>{props.count}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.props.count}</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    );
+  }
 }
 let count = 1;
 const rootElement = document.getElementById("root");
