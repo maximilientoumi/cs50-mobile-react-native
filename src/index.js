d86445511aba28bd95f80b25f7d8cf10ae6aebd3
max#237 <tmguil2015inde@gmail.com>
2018-07-10 22:50:08 +0530

    arow  func
    usin arow func onClick

--- src/index.js
+++ src/index.js
@@ -4,24 +4,23 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       count: 0
-    }
-  this.handleCount = this.handleCount.bind(this)
+    };
+    this.handleCount = this.handleCount.bind(this);
   }
-  handleCount(){
+  handleCount() {
     this.setState({
       count: this.state.count + 1
-    })
+    });
   }
   render() {
-  
     return (
       <div className="App">
         <h1>{this.state.count}</h1>
-        <button onClick={this.handleCount}>count</button>
+        <button onClick={() => this.handleCount()}>count</button>
       </div>
     );
   }
